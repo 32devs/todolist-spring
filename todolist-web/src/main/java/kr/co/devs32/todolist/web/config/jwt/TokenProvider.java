5f31451caf81d8d831715acf15bf8fe4cd1b72ff
jhpark <pjhwin1@naver.com>
2023-07-30 17:39:44 +0900

    스프링 시큐리티 + JWT 테스트  TDL-13

--- todolist-web/src/main/java/kr/co/devs32/todolist/web/config/jwt/TokenProvider.java
+++ todolist-web/src/main/java/kr/co/devs32/todolist/web/config/jwt/TokenProvider.java
@@ -1,14 +1,20 @@
 package kr.co.devs32.todolist.web.config.jwt;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Header;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import kr.co.devs32.todolist.web.domain.User;
+import kr.co.devs32.todolist.web.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
 import java.time.Duration;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Set;
 
 @RequiredArgsConstructor
 @Service
@@ -20,7 +26,7 @@ public class TokenProvider {
         return makeToken(new Date(now.getTime() + expiredAt.toMillis()), user);
     }
 
-    //JWT 토큰 생성 메소드
+    //JWT 토큰 생성 메서드
     private String makeToken(Date expiry, User user) {
         Date now = new Date();
 
@@ -28,12 +34,48 @@ public class TokenProvider {
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE) // 헤더 typ : JWT
                 //내용 iss : pjhwin1@gmail.com
                 .setIssuer(jwtProperties.getIssuer())
-                .setIssuedAt(now)
-                .setExpiration(expiry)
-                .setSubject(user.getEmail())
-                .claim("id", user.getId())
+                .setIssuedAt(now)   // 현재시간
+                .setExpiration(expiry)  //expiry 멤버 변숫값
+                .setSubject(user.getEmail()) //유저 이메일
+                .claim("id", user.getId()) //유저 아이디
                 //서명 : 비밀값과 함께 해시값을 HS256 방식으로 암호화
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
                 .compact();
     }
+
+    //JWT 토큰 유효성 검증 메서드
+    public boolean vaildToken(String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey()) //비밀값으로 복호화
+                    .parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    //토큰 기반으로 인증 정보를 가져오는 메서드
+    public Authentication getAuthentication(String token) {
+        Claims claims = getClaims(token);
+        Set<SimpleGrantedAuthority> authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return new UsernamePasswordAuthenticationToken(new org.springframework.
+                security.core.userdetails.User(claims.getSubject(),"",authorities), token, authorities);
+    }
+
+    //토큰 기반으로 유저 ID를 가져오는 메서드
+    public Long getUserId(String token) {
+        Claims claims = getClaims(token);
+        return claims.get("id", Long.class);
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()    //클레임 조회
+                .setSigningKey(jwtProperties.getSecretKey())
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
 }
--- todolist-web/src/main/java/kr/co/devs32/todolist/web/service/UserService.java
+++ todolist-web/src/main/java/kr/co/devs32/todolist/web/service/UserService.java
@@ -1,6 +1,6 @@
 package kr.co.devs32.todolist.web.service;
 
-import kr.co.devs32.todolist.web.domain.User;
+import kr.co.devs32.todolist.web.entity.User;
 import kr.co.devs32.todolist.web.dto.AddUserRequest;
 import kr.co.devs32.todolist.web.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -11,13 +11,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public Long save(AddUserRequest dto) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
         return userRepository.save(User.builder()
                 .email(dto.getEmail())
                 //패스워드 암호화
-                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .password(encoder.encode(dto.getPassword()))
                 .build()).getId();
+    }
+
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 }
