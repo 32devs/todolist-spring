3c050213aacea748d55dd82864b4deb7f757a82a
jparangdev <jparangdev@gmail.com>
2024-05-04 10:02:17 +0900

    Circle CI 업데이트

--- script/health_check.sh
+++ script/health_check.sh
@@ -1,23 +1,10 @@
-exit 1
-~
-                                                                                                                                                                                10,70         All
-echo "----- pull latest image -----"
-APP_NAME=todolist-springboot
-docker pull devs32/$APP_NAME
-
-echo "----- stop container if exist container -----"
-docker stop $APP_NAME
-docker rm $APP_NAME
-
-echo "----- start todolist-springboot docker image  -----"
-docker run -d -p 10001:8080 -e SPRING_PROFILE=develop -e databaseUsername=postgres.dddqpitqgcvuxuqnrvty -e databasePassword=Devs#@1989!! --name $APP_NAME devs32/todolist-springboot
+#!/bin/bash                                                                                                                                                                       10,70         All
 
 APP_URL="http://localhost:10001/hello" # 스프링 부트 확인 주소
-MAX_ATTEMPTS=30 # 최대 시도 횟수
+MAX_ATTEMPTS=10 # 최대 시도 횟수
 SLEEP_SECONDS=10 # 각 시도 사이의 대기 시간 (초)
 
 attempt=0
-
 echo "----- check spring boot start -----"
 while [ $attempt -lt $MAX_ATTEMPTS ]; do
     response_code=$(curl --write-out "%{http_code}\n" --silent --output /dev/null $APP_URL)
