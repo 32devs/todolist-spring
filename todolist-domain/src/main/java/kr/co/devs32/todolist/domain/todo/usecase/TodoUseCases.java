df567aa5efa107119d4cca9f43889391121735b5
jhpark <pjhwin1@naver.com>
2024-05-12 14:41:31 +0900

    투두리스트 유즈케이스 정의

--- todolist-domain/src/main/java/kr/co/devs32/todolist/domain/todo/usecase/TodoUseCases.java
+++ todolist-domain/src/main/java/kr/co/devs32/todolist/domain/todo/usecase/TodoUseCases.java
@@ -12,4 +12,14 @@ public interface TodoUseCases {
 
 	// 할일 조회
 	List<Todo> search(TodoSearchQuery query);
+
+	// 할일 수정
+	Todo update(Todo todo);
+
+	// 할일 삭제
+	Todo remove(Todo todo);
+
+	// 할일 완료
+	Todo complete(Todo todo);
+
 }
